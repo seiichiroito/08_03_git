7f1518a5accce41db0f5546dc8179c2a58e65d67
Toru Tamaki <tamaki.toru@nitech.ac.jp>
2021-02-22 11:53:55 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+def myfunc(x, y):
+    z = 2 * x - y
+    return z
+
+
+def myfunc_2(a):
+    for i in range(5):
+        myfunc(a[i], a[i + 1])
+
+def myfunc_3(a):
+    for i in range(5):
+        myfunc(a[i], a[i + 1])
+
+
+def main():
+    a = [1, 2, 3, 4, 5]
+    for _ in range(100):
+        myfunc_2(a)
+    for _ in range(300):
+        myfunc_3(a)
+
+
+if __name__ == '__main__':
+    main()
