eb88fd1882c3974b687894ae5578658a863716e3
Toru Tamaki <tamaki.toru@nitech.ac.jp>
2021-02-22 12:25:50 +0900

    second commit

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ def myfunc(x, y):
     return z
 
 
-def myfunc_2(a):
-    for i in range(5):
-        myfunc(a[i], a[i + 1])
-
 def myfunc_3(a):
     for i in range(5):
         myfunc(a[i], a[i + 1])
 
+def myfunc_4(a):
+    z = a[1] + a[2]
 
 def main():
     a = [1, 2, 3, 4, 5]
